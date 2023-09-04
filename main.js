466539e3762f274b7a01a842a27729be64db3941
Rishabh <rishabhtiwari26>
2023-09-04 12:39:17 +0530

    dom2

---
+++ main.js
@@ -0,0 +1,6 @@
+var ht=document.getElementById('main-header');
+ht.style.borderBottom= 'solid 3px #000'
+let add_items=document.getElementById("main").getElementsByClassName('title')[0]
+
+add_items.style.color='green'
+add_items.innerHTML='<strong>Add Items</strong>'
