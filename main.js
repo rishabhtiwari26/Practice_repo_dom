5bede86a23ee45ef2f7bf32f026f2b4e09bac867
= <rishabhtiwari26>
2023-09-04 15:49:31 +0530

    3rd item bacckground green and items get bolded

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ let add_items=document.getElementById("main").getElementsByClassName('title')[0]
 add_items.style.color='green'
 add_items.innerHTML='<strong>Add Items</strong>'
 // new line comment
+
+{
+    let items=document.getElementsByClassName('list-group-item');
+    console.log(items)
+    for (let i=0; i<items.length;i++){
+        items[i].style.fontWeight = 'bold'
+    }
+    items[2].style.backgroundColor='green'
+}
