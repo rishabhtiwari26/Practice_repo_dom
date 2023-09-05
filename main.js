7ef2c59b762f21ae1b18349c4273759e296330b4
= <rishabhtiwari26>
2023-09-05 13:43:59 +0530

    filter

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function addItems(e){
     e.preventDefault();
 
     var newItem=document.getElementById('item').value
+    var itemdescription=document.getElementById("itemdescription").value
     var li=document.createElement('li')
     li.className='list-group-item'
-    li.appendChild(document.createTextNode(newItem))
+    li.appendChild(document.createTextNode(newItem+' '))
+    li.appendChild(document.createTextNode(itemdescription))
     var button=document.createElement('button')
     button.className='btn btn-danger btn-sm float-right delete'
     button.appendChild(document.createTextNode('X'))
@@ -25,4 +27,26 @@ function removeItems(e){
             itemList.removeChild(li)
         }
     }
+}
+
+var filter = document.getElementById('filter')
+filter.addEventListener('keyup',filterItems)
+function filterItems(e){
+    var text=e.target.value.toLowerCase();
+    var li = document.getElementsByTagName('li')
+    Array.from(li).forEach(function(item){
+        let itemName=item.firstChild.textContent;
+        if (item.childNodes[1].className='list-group-item'){
+            var itemdescript = item.childNodes[1].textContent
+        }
+
+        console.log(itemdescript)
+        if (itemName.toLowerCase().indexOf(text)!=-1 || itemdescript.toLowerCase().indexOf(text)!=-1) {
+            item.style.display='block'
+        }
+        else{
+            item.style.display='none'
+        }
+    })
+    // // console.log((li))
 }
